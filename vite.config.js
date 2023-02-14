548870952c531eae1970f405d4c3869fdb18cf44
AyloKieran <kieran@aylo.net>
2023-02-14 09:49:47 +0000

    Use GH Pages

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ let _time = `${_now.getHours().toString().padStart(2, '0')}${_now.getMinutes().t
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   define: {
     '__APP_VERSION__': JSON.stringify(`v${process.env.npm_package_version}-${_date}-${_time}`),
   },
