ebeb0ac94e330277e4e72b739cf3184b18bb867c
Kieran Noble <kieran@erno.co>
2025-05-04 14:53:05 +0100

    Fix page load issues

--- kierannoble.dev/UI/Layout/Blackout/_Blackout.js
+++ kierannoble.dev/UI/Layout/Blackout/_Blackout.js
@@ -3,8 +3,10 @@
     const blackout = document.getElementById("blackout");
     const CSS_BLACKOUT_SHOWN = "blackout--shown";
 
-    window.addEventListener("pageshow", function (event) {
-        blackout.classList.remove(CSS_BLACKOUT_SHOWN);
+    window.addEventListener("load", function () {
+        window.addEventListener("pageshow", function (event) {
+            blackout.classList.remove(CSS_BLACKOUT_SHOWN);
+        }); 
     });
 
     window.addEventListener("beforeunload", function (event) {
