1f1d239ba9146bcc1dc6d89365573e456fdbf38b
Kieran Noble <kieran@erno.co>
2025-10-12 18:52:39 +0100

    Add missing file

---
+++ kierannoble.dev/Managers/Media/R2StorageOptions.cs
@@ -0,0 +1,10 @@
+namespace kierannoble.dev.Managers.Media;
+
+public class R2StorageOptions
+{
+    public string Endpoint { get; set; } = string.Empty;
+    public string AccessKey { get; set; } = string.Empty;
+    public string SecretKey { get; set; } = string.Empty;
+    public string BucketName { get; set; } = string.Empty;
+    public string CdnBaseUrl { get; set; } = string.Empty;
+}
